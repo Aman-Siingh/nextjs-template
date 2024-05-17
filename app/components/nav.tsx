aebac44ac3db58db144791cc2b2a96d37c554d9f
Pooja Singh <amansiingh15@gmail.com>
2024-05-18 00:08:20 +0530

    update

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Navbar() {
   return (
-    <nav className="bg-gray-800 p-4 flex justify-around items-center">
+    <nav className=" bg-black p-4 flex justify-around items-center">
       <a href="/" className="text-white text-lg px-4 py-2 hover:bg-gray-700 rounded">Home</a>
       <a href="/about" className="text-white text-lg px-4 py-2 hover:bg-gray-700 rounded">About</a>
       <a href="/services" className="text-white text-lg px-4 py-2 hover:bg-gray-700 rounded">Services</a>
